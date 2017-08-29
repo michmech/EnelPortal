3cc0a686f41c714ff25de8a0fcf42fddba39b3bd
Michal Boleslav Měchura <valselob@gmail.com>
2017-08-29 11:25:53 +0200

    added Lombard and Occitan

--- Website/Data.cs
+++ Website/Data.cs
@@ -8,12 +8,13 @@ namespace Website
 	{
 		public static string GetConnectionString()
 		{
+			//return "";
 			return @"Data Source=(local)\SQLEXPRESS;Initial Catalog=enelportal;Integrated Security=true;";
 		}
 		public static string GetTrackingCode()
 		{
-			return "";
 			//return "<!-- Start of StatCounter Code for Default Guide--><script type=\"text/javascript\"> var sc_project=10391388; var sc_invisible=1; var sc_security=\"51c01d0b\"; var scJsHost = ((\"https:\" == document.location.protocol) ? \"https://secure.\" : \"http://www.\"); document.write(\"<sc\"+\"ript type='text/javascript' src='\" + scJsHost + \"statcounter.com/counter/counter.js'></\"+\"script>\"); </script><noscript><div class=\"statcounter\"><a title=\"site stats\" href=\"http://statcounter.com/free-web-stats/\" target=\"_blank\"><img class=\"statcounter\" src=\"http://c.statcounter.com/10391388/0/51c01d0b/1/\" alt=\"site stats\"></a></div></noscript><!-- End of StatCounter Code for Default Guide -->";
+			return "";
 		}
 	}
 	
@@ -135,6 +136,9 @@ namespace Website
 			this.languages.Add(new Language("vep", "Vepsian", "", false));
 			this.languages.Add(new Language("yi", "Yiddish", "", false));
 			this.languages.Add(new Language("br", "Breton", "", false));
+
+			this.languages.Add(new Language("lmo", "Lombard", "", false));
+			this.languages.Add(new Language("oc", "Occitan", "", false));
 
 			this.languages.Add(new Language("sqk", "Albanian Sign Language", "", false, true));
 			this.languages.Add(new Language("aen", "Armenian Sign Language", "", false, true));
